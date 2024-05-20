11434bb721b68a498ac6681f363924766e1f8265
KantasOnur <kantasonurkilic@gmail.com>
2024-05-20 09:41:55 -0700

    Learned how offsetof macro works and, finally understood vertexattribarray as well as how indices are used. Next is to put vao, vbo, and ibo into its own classes

--- objLoader/src/main.cpp
+++ objLoader/src/main.cpp
@@ -5,8 +5,10 @@
 #include <string>
 #include <sstream>
 #include <fstream>
+#include <vector>
 
 #include "Shader.hpp"
+
 
 int main(void)
 {
@@ -40,24 +42,46 @@ int main(void)
         std::cout << "failed" << std::endl;
     }
     
-    float positions[] = {
-            -.5f, -.5f, 1.0, 0.0, 0.0, 1.0,
-            .0f, .5f,   0.0, 1.0, 0.0, 1.0,
-            .5f, -.5f,  0.0, 0.0, 1.0, 1.0
-        };
+    struct Vertex
+    {
+        float position[2];
+        float color[4];
+    };
+    
+    std::vector<Vertex> v = {
+        {{-.5f, -.5f}, {1.0, 0.0, 0.0, 1.0}},
+        {{.5f, -.5f}, {0.0, 1.0, 0.0, 1.0}},
+        {{.5f, .5f}, {0.0, 0.0, 1.0, 1.0}},
+        
+        //{{-.5f, -.5f}, {0.0, 1.0, 0.0, 1.0}},
+        {{-.5f, .5f}, {1.0, 0.0, 0.0, 1.0}}
+        //{{.5f, -.5f}, {0.0, 0.0, 1.0, 1.0}}
+    };
+    
+    
+    std::vector<unsigned int> indices = {
+        0, 1, 2,
+        2, 3, 0
+    };
     
     unsigned int VBO, VAO;
     glGenBuffers(1, &VBO);
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, v.size() * sizeof(Vertex), v.data(), GL_STATIC_DRAW);
 
+    
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices.data(), GL_STATIC_DRAW);
+    
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, position));
 
     glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 6 *sizeof(float), (void *)(2 * sizeof(float)));
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, color));
     
     Shader shader("shaders/Basic.vert", "shaders/Basic.frag");
     shader.Bind();
@@ -68,8 +92,8 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, int(indices.size()), GL_UNSIGNED_INT, nullptr);
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
